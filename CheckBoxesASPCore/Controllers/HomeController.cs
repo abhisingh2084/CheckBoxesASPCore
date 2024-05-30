f5e287fc45c5be456c5e2e86e40b34b6d9aac1fc
abhishek kumar <abhisingh2084@gmail.com>
2024-05-30 17:22:30 +0530

    Multiple checkbox logic added

--- CheckBoxesASPCore/Controllers/HomeController.cs
+++ CheckBoxesASPCore/Controllers/HomeController.cs
@@ -13,20 +13,50 @@ namespace CheckBoxesASPCore.Controllers
             _logger = logger;
         }
 
+        //public IActionResult Index()
+        //{
+        //    var model = new ViewModel()
+        //    {
+        //        AcceptTerms = false,
+        //        Text = "I Accepts The terms",
+        //    };
+        //    return View(model);
+        //}
+
         public IActionResult Index()
         {
             var model = new ViewModel()
             {
-                AcceptTerms = false,
-                Text = "I Accepts The terms",
+                CheckBoxes = new List<CheckBoxOption>
+                {
+                    new CheckBoxOption()
+                    {
+                        IsChecked = true,
+                        Text = "Cricket",
+                        Value = "Cricket",
+                    },
+                    new CheckBoxOption()
+                    {
+                        IsChecked = false,
+                        Text = "Football",
+                        Value = "Football",
+                    },
+                    new CheckBoxOption()
+                    {
+                        IsChecked = true,
+                        Text = "Hokey",
+                        Value = "Hokey",
+                    },
+                }
+
             };
             return View(model);
         }
         [HttpPost]
         public IActionResult Index(ViewModel data)
         {
-            var value = data.AcceptTerms;
-            return View(value);
+            //var value = data.AcceptTerms;
+            return View();
         }
 
         public IActionResult Privacy()
--- CheckBoxesASPCore/Models/ViewModel.cs
+++ CheckBoxesASPCore/Models/ViewModel.cs
@@ -2,7 +2,13 @@
 {
     public class ViewModel
     {
-        public bool AcceptTerms { get; set; }
-        public string Text { get; set; }
+        public List<CheckBoxOption> CheckBoxes { get; set; }
+        public List<string> Sports { get; set; }
     }
+
+    //public class ViewModel
+    //{
+    //    public bool AcceptTerms { get; set; }
+    //    public string Text { get; set; }
+    //}
 }
